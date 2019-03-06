983141037ff613aedd54c13324711d5d20c66590
JingyZhu <jingyz@umich.edu>
2019-03-06 18:42:32 +0100

    SearchScreen done

--- frontend/BuildIT/screens/SearchScreen.js
+++ frontend/BuildIT/screens/SearchScreen.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text, FlatList, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, FlatList} from 'react-native';
+import {Icon } from 'react-native-elements';
 import SearchQR from '../components/SearchQR';
 
 export default class SearchScreen extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            searchText: ''
         }
     }
 
+    updateSearchText(text){
+        this.setState(
+        {searchText: text}
+        )
+    }
+
+
+    searchResult(searchText){
+        if (searchText === '' )
+            return [
+                {name: 'Table', id: 0},
+                {name: 'Bed', id: 1},
+                {name: 'Lamp', id: 2}
+            ]
+        else return [];
+    }
+
+
     render() {
+        let sr = this.searchResult(this.state.searchText);
         return (
-            <SearchQR naviFunc={this.props.navigation.navigate.bind(this)} naviScreen={'Home'} screen={'Search'} />
+            <View>
+                <SearchQR naviFunc={this.props.navigation.navigate.bind(this)} naviScreen={'Home'} screen={'Search'} />
+                <FlatList  
+                    data={sr}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({item}) => 
+                        <View style={styles.searchItem}>
+                            <Icon name='clock-outline' type='material-community' containerStyle={{flex: 0.1}}/>
+                            <Text style={{flex: 0.9, fontSize: 16 }}>{item.name}</Text>
+                        </View>
+                    }
+                />
+            </View>
         );
     }
 } 
+
+const styles = StyleSheet.create({
+    searchItem: {
+        borderColor: '#d7dadd',
+        flexDirection: 'row',
+        margin: 10
+    },
+})

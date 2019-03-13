9f57a0a85ff9eb2cd058e00bcbf0cca4ba1f3d8d
xiaoyuf <happyvickyfxy@163.com>
2019-03-13 19:48:45 -0400

    func frame

--- backend/buildIT/views.py
+++ backend/buildIT/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt 
+import json
 
 # Create your views here.
+
+def recommend(request):
+    pass
+
+def search(request):
+    pass
+
+def comment(request):
+    pass
+
+def tools(request):
+    pass
+
+def step_manual(request):
+    pass
+
+def cv_upload(request):
+    pass
+
+def video(request):
+    pass

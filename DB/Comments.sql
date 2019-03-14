8da94a34cecba07260b90b81cfb9daa30986e0ba
yimzhou <yimzhou@umich.edu>
2019-03-13 20:33:29 -0400

    remove all

--- DB/Comments.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Comments (
-    SID int NOT NULL,
-    Content varchar(255) NOT NULL,
-    FOREIGN KEY (SID) REFERENCES Steps(SID),
-);
-
--- DB/Components.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Components (
-    CID int NOT NULL AUTO_INCREMENT,
-    Name varchar(255) NOT NULL,
-    Description  varchar(255),
-    PRIMARY KEY (CID)
-);
-

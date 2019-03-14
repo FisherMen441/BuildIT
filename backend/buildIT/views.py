160319d9ca2d991704741dfaedb1b4342270398f
xiaoyuf <happyvickyfxy@163.com>
2019-03-13 20:55:26 -0400

    backend fetch tool list

--- backend/buildIT/views.py
+++ backend/buildIT/views.py
@@ -16,7 +16,25 @@ def comment(request):
     pass
 
 def tools(request):
-    pass
+    if request.method != 'GET':
+		return HttpResponse(status=404)
+	furniture_id = request.GET.get('furniture_id')
+    step = request.GET.get('step')
+	cursor = connection.cursor()
+    cursor.execute("SELECT TID FROM Tools_needed WHERE FID=? AND SID=?", (furniture_id, step))
+    tool_ids = cursor.fetchall()
+    tool_list = []
+    for tool_id in tool_ids:
+        cursor.execute("SELECT * FROM Tools WHERE TID=?", (tool_id['TID']))
+        info = cursor.fetchall()[0]
+        tool_info = {
+            'Name': info['Name'],
+            'Img_url': info['Img_url'],
+            'Description': info['Description']
+        }
+        tool_list += [tool_info]
+    response = {'tool_list': tool_list}
+	return JsonResponse(response)
 
 def step_manual(request):
     pass

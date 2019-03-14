2d16e8b7a86c5925228a62c11820b734bb67acfe
Jiaxin Ye <jiaxinye@JiaxindeMacBook-Pro.local>
2019-03-13 21:08:49 -0400

    dbsql

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from buildIT import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'api/recommend/$', views.recommend, name='recommend')
 ]

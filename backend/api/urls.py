51c551460bd9de4b85005997d79527ea6d9b0906
yimzhou <35083880+yimzhou@users.noreply.github.com>
2019-03-15 23:25:33 -0400

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'api/tools/$', views.tools, name='tools'),
     url(r'api/videos/$', views.videos, name='videos'),
     url(r'api/manual/$', views.step_manual, name='manual'),
-    url(r'api/furniture_info/$', views.furniture_info, name='furniture_info')
+    url(r'api/furniture_info/$', views.furniture_info, name='furniture_info'),
+    url(r'api/search/$', views.search, name='search')
 ]

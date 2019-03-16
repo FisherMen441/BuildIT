b3fe96cb172c49b5e7665ec1adc5634f674e358e
yimzhou <yimzhou@umich.edu>
2019-03-15 23:02:58 -0400

    search try

--- frontend/BuildIT/screens/SearchScreen.js
+++ frontend/BuildIT/screens/SearchScreen.js
@@ -8,7 +8,9 @@ export default class SearchScreen extends React.Component {
         super();
         this.state = {
             searchText: ''
-        }
+        };
+        this.updateSearchText = this.updateSearchText.bind(this);
+        this.searchResult = this.searchResult.bind(this);
     }
 
     updateSearchText(text) {
@@ -25,15 +27,41 @@ export default class SearchScreen extends React.Component {
                 { name: 'Bed', id: 1 , uri: 'https://cdn.shopify.com/s/files/1/2660/5106/files/LR-2-Main_159cda8c-8447-4d3b-888b-0bc8b8999dd2_960x.jpg'},
                 { name: 'Lamp', id: 2, uri: 'https://cdn.shopify.com/s/files/1/2660/5106/files/LR-2-Main_159cda8c-8447-4d3b-888b-0bc8b8999dd2_960x.jpg' }
             ]
-        else return [];
+        else {
+            console.log('search: ', this.state.searchText);
+            fetch(
+                'http://0.0.0.0:8000/api/search/?search_text=' 
+                + this.state.searchText,
+                {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            .then(response => {
+                if (!response.stateText == 'OK')
+                    throw Error("Not 200 status code");
+            })
+            .then((data)=>{
+                return_list = []
+                data.forEach((d) => {return_list.push({name: d.name, id: d.id})});
+                return return_list;
+            })
+            .catch(error => console.log('Error: ', error))
+        };
     }
 
 
     render() {
         let sr = this.searchResult(this.state.searchText);
+        console.log('search: ', this.state.searchText);
         return (
             <View>
-                <SearchQR naviFunc={this.props.navigation.navigate.bind(this)} naviScreen={'Home'} screen={'Search'} />
+                <SearchQR 
+                    updateSearchText={this.updateSearchText.bind(this)} 
+                    naviFunc={this.props.navigation.navigate.bind(this)} 
+                    naviScreen={'Home'} 
+                    screen={'Search'} />
                 <FlatList
                     data={sr}
                     keyExtractor={item => item.id.toString()}
@@ -50,8 +78,7 @@ export default class SearchScreen extends React.Component {
                                 <Text style={{ flex: 0.9, fontSize: 16 }}>{item.name}</Text>
                             </View>
                         </TouchableOpacity>
-                    }
-                />
+                    } />
             </View>
         );
     }

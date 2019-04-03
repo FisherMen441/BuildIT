161f4723883a39d73edde229ebff4f68086a1624
xiaoyuf <happyvickyfxy@163.com>
2019-04-03 14:36:02 -0400

    modify sift to accept multiple images

--- cv/sift.py
+++ cv/sift.py
@@ -19,61 +19,62 @@ def preprocess(file):
         dImg.save(file)
 
 
-def recognize_from_image(file1='query.jpeg', file2='src.jpeg'):
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
+def recognize_from_image(files=['2.jpeg', '1.jpeg']):
+    origin_photo = files[0]
     MIN_MATCH_COUNT = 10
+    img_colored = cv2.imread(files[0], cv2.IMREAD_COLOR)
+    origin = cv2.imread(files[0], 0) # trainImage
 
     # Preprocess the pictures to accelerate 
-    preprocess(file1)
-    preprocess(file2)
+    for item in files:
+        preprocess(item)
 
-    img1 = cv2.imread(file1, 0)  # queryImage
-    img2 = cv2.imread(file2, 0)  # trainImage
-    img2_colored = cv2.imread(file2, cv2.IMREAD_COLOR)
+    i = 1
+    while i < len(files):
+        img = cv2.imread(files[i], 0)  # queryImage
+        
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
 
-    # Initiate SIFT detector
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img,None)
+        kp2, des2 = sift.detectAndCompute(origin,None)
 
 
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1, des2, k=2)
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
 
-    matches = flann.knnMatch(des1, des2, k=2)
+        if len(good) > MIN_MATCH_COUNT:
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m, n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
 
-    if len(good) > MIN_MATCH_COUNT:
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            h, w = img.shape
+            pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+            dst = cv2.perspectiveTransform(pts, M)
 
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        matchesMask = mask.ravel().tolist()
+            origin = cv2.polylines(img_colored, [np.int32(dst)], True, (255,0,0), 7, cv2.LINE_AA)
 
-        h, w = img1.shape
-        pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
-        dst = cv2.perspectiveTransform(pts, M)
+        else:
+            print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
+            matchesMask = None
 
-        img2 = cv2.polylines(img2_colored, [np.int32(dst)], True, (255,0,0), 7, cv2.LINE_AA)
-
-    else:
-        print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-        matchesMask = None
+        i += 1
 
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(img2)
+    plt.imshow(origin)
     plt.subplots_adjust(bottom = 0)
     plt.subplots_adjust(top = 1)
     plt.subplots_adjust(right = 1)
